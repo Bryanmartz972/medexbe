c14059d5fc0ae22ecc6c6b2f5472f5be85f01ac0
Bryanmartz972 <bryanmartz972@gmail.com>
2022-01-31 18:55:07 -0600

    Agregado de headerVerifyMiddleware

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pacientesRoutes = require('./pacientes/pacientes');
-const expedientesRoutes = require('./expedientes/expedientes');
+const { verifyApiHeaderToken } = require("./headerVerifyMiddleware");
+const pacientesRoutes = require("./pacientes/pacientes");
+const expedientesRoutes = require("./expedientes/expedientes");
 
 //Rutas
-router.use('/pacientes', pacientesRoutes);
-router.use('/expedientes', expedientesRoutes);
+//Middlewares
+router.use("/pacientes", verifyApiHeaderToken , pacientesRoutes);
+router.use("/expedientes", expedientesRoutes);
 
 module.exports = router;
